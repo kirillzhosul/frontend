c0b93b5b9560b5481a9dea87f045e4e81b0dc7d5
Kirill Zhosul <kirillzhosul@yandex.com>
2022-03-17 09:35:10 +0400

    Bootstrap

--- src/page/src/index.js
+++ src/page/src/index.js
@@ -1,6 +1,12 @@
+// Libraries.
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Main app.
 import App from './App';
+
+// Bootstrap.
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
