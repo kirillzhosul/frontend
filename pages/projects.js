3e703243bd2fadd366efd9ee79b7a397e5fd2879
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-10-30 13:34:47 +0400

    Update projects

--- pages/projects.js
+++ pages/projects.js
@@ -37,14 +37,10 @@ export default function Projects() {
           </div>
           <p className="bc-text">
             <b>
-              <i>My projects</i>
+              <i>My GitHub projects</i>
             </b>
             <br />
-            <a href="https://github.com/florgon/gatey-sdk-py">
-              florgon/gatey-sdk-py
-            </a>
-            , Python library that works with my error logging service and doing
-            logging errors.
+            <b>&nbsp;APIs and Web</b>
             <br />
             <a href="https://github.com/florgon/api">florgon/api</a>, FastAPI
             backend for my website with OAuth and other stuff.
@@ -52,18 +48,13 @@ export default function Projects() {
             <a href="https://github.com/kirillzhosul/shop">kirillzhosul/shop</a>
             , Merchandise shop for the competition.
             <br />
-            <a href="https://github.com/kirillzhosul/hhru">kirillzhosul/hhru</a>
-            , hh.ru simple wrapper-library. Works with HeadHunter API.
+            <b>&nbsp;Tools and libs</b>
             <br />
-            <a href="https://github.com/kirillzhosul/cookie-widget">
-              kirillzhosul/cookie-widget
+            <a href="https://github.com/florgon/gatey-sdk-py">
+              florgon/gatey-sdk-py
             </a>
-            , Widget about using cookies hosted on GitHub pages.
-            <br />
-            <a href="https://github.com/kirillzhosul/next-yandex-metrika">
-              kirillzhosul/next-yandex-metrika
-            </a>
-            , Small component that allows to use Yandex.Metrika in Next.js
+            , Python library that works with my error logging service and doing
+            logging errors.
             <br />
             <a href="https://github.com/kirillzhosul/dotfiles">
               kirillzhosul/dotfiles
@@ -71,35 +62,57 @@ export default function Projects() {
             , Collection of scripts / configs files that I actively use during
             work (Docker / Nginx / etc)
             <br />
-            <a href="https://github.com/kirillzhosul/matrix-webcam">
-              kirillzhosul/matrix-webcam
-            </a>
-            , Page that makes your webcam behave like matrix. (Hosted on GitHub
-            pages).
+            <a href="https://github.com/kirillzhosul/hhru">kirillzhosul/hhru</a>
+            , hh.ru simple wrapper-library. Works with HeadHunter API.
             <br />
-            <a href="https://github.com/kirillzhosul/django-stripe">
-              kirillzhosul/django-stripe
+            <a href="https://github.com/kirillzhosul/next-yandex-metrika">
+              kirillzhosul/next-yandex-metrika
             </a>
-            , Test task for one of the companies. Django simple *shop* with
-            Stripe.
+            , Small component that allows to use Yandex.Metrika in Next.js
+            <br />
+            <b>&nbsp;Looking for more?</b>
+            <br />
+            <i>
+              There is not all of my projects, some are not listed here (old
+              ones or Game Maker), <br />
+              you can checkout all my projects on my GitHub{" "}
+              <a href="https://github.com/kirillzhosul">[link, click]</a>.
+            </i>
           </p>
           <br />
           <p className="bc-text">
             <b>
-              <i>My websites</i>
+              <i>My deployed projects</i>
             </b>
             <br />
-            <a href="https://kirillzhosul.site">kirillzhosul.site</a>, This
-            website, personal page.
+            <a href="https://api.florgon.space/v1">Florgon API</a> [
+            <a href="https://github.com/florgon/api">source</a>] , Root API for
+            my project, OAuth etc.
             <br />
-            <a href="https://kirillzhosul.github.io">kirillzhosul.github.io</a>
-            , Storage for GitHub pages projects.
+            <a href="https://kirillzhosul.github.io/matrix-webcam">
+              Matrix Webcam
+            </a>{" "}
+            [<a href="https://github.com/kirillzhosul/matrix-webcam">source</a>]
+            , Page that makes your webcam behave like matrix.
+            <br />
+            <a href="https://kirillzhosul.github.io/cookie-widget">
+              Cookie Widget
+            </a>{" "}
+            [<a href="https://github.com/kirillzhosul/cookie-widget">source</a>]
+            , Simple static cookie widget that notifies about cookie usage.
+            <br />
+            <a href="https://kirillzhosul.site/tests/stripe">Django Stripe</a> [
+            <a href="https://github.com/kirillzhosul/django-stripe">source</a>],
+            Test solution for Stripe in Django (mini *shop*).
+          </p>
+          <br />
+          <p className="bc-text">
+            <b>
+              <i>My other websites</i>
+            </b>
             <br />
             <a href="https://florgon.space">florgon.space</a>, My website with
             different services and SSO (OAuth).
-            <br />
-            <a href="https://cc.florgon.space">cc.florgon.space</a>, Link
-            shortener.
             <br />
             <a href="https://gatey.florgon.space">gatey.florgon.space</a>, Error
             logging / monitoring service.
