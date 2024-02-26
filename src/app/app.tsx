720efc63d47e52e1c1e5ad458481f0d1295f24ef
Kirill Zhosul <kirillzhosul@yandex.com>
2024-02-26 21:21:35 +0400

    feat: :art: Add title

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,15 +2,21 @@ import type { AppProps } from "next/app";
 import { AppProvider } from "./app-provider";
 import { Inter } from "next/font/google";
 import clsx from "clsx";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
 export function App({ Component, pageProps }: AppProps) {
   return (
-    <AppProvider>
-      <div className={clsx(inter.className, "")}>
-        <Component {...pageProps} />
-      </div>
-    </AppProvider>
+    <div>
+      <Head>
+        <title>Kirill Zhosul</title>
+      </Head>
+      <AppProvider>
+        <div className={clsx(inter.className, "")}>
+          <Component {...pageProps} />
+        </div>
+      </AppProvider>
+    </div>
   );
 }
