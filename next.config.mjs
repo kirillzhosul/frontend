ccd1f0d92d3217bbd34342cda1be514cc543be08
Kirill Zhosul <kirillzhosul@yandex.com>
2024-02-26 20:41:01 +0400

    feat(deploy): :fire: Remove GitHub pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,4 @@ let nextConfig = {
   reactStrictMode: true,
 };
 
-if (process.env.NODE_ENV === "production") {
-  nextConfig.basePath = "/frontend";
-  nextConfig.assetPrefix = "/frontend";
-  nextConfig.output = "export";
-} else {
-}
-
 export default nextConfig;
